5f6a29ec4a04a63ee341f9c0b5edabc9f99beea8
Priya-Rathor <priya.rathor.266393@gmail.com>
2025-01-27 13:38:18 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         {/* Define routes for components */}
-        <Route path="/" element={<FeedbackApp/>} />
+        <Route path="/" element={<Signin/>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/writing" element={<EssayWriting />} />
